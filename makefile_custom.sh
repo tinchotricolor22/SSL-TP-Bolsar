d779d9ea418220ee317607df1ffba969efbb285b
mamoralesMELI <martin.morales@mercadolibre.com>
2019-08-25 21:38:58 -0300

    modify makefile

--- makefile_custom.sh
+++ makefile_custom.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-array=`find . -name "*.c" -not -path "*custom_build/*" -not -path "*main.c*"`
+array=`find src/. -name "*.c" -not -path "*main.c*"`
 executableFiles=""
 
 for i in $array
 do
-	oldFullPath=${i#"./"}
+	oldFullPath=${i#"src/"}
 	oldFullPath="custom_build/${oldFullPath%'.c'}"
 	fileNameObject="${oldFullPath##*/}.o"
 	directoryPath=${oldFullPath%/*}
@@ -18,6 +18,6 @@ do
     executableFiles="$executableFiles $newFullPath "
 done
 
-gcc -c main.c -o 'custom_build/main.o'
+gcc -c src/main.c -o 'custom_build/main.o'
 executableFiles="$executableFiles custom_build/main.o"
-gcc -o executable $executableFiles
+gcc -o SS_TP_BOLSAR $executableFiles
